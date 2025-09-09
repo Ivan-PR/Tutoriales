d3df5f7e616c2562362ea33e0ddae1e49194370f
Ivan-PR <ipr.prieto@gmail.com>
2025-09-10 01:25:38 +0200

    Tipos de datos y Introduccion al lenguaje

---
+++ DotNet/Tutorial_01/OpenWebinarsConsoleApp/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenWebinarsConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        static void Declarations()
+        {
+            int number = 0;
+            double numberDecimals = 0.0d;
+            bool yayOrNay = true;
+            string helloWorld = string.Empty;
+        }
+
+        static void Conditions() {
+            int number = 0;
+            number += 2;
+            //number => 2
+
+            number -= 2;
+            //number => 0
+
+            //Si
+            if (number == 0)
+            {
+                //Hacer algo
+            }
+            //Sino Si
+            else if (number > 0)
+            {
+                //Hacer otra cosa
+            }
+            //Sino
+            else
+            {
+                //Hacer otra cosa
+            }
+        }
+
+        static void Loops() {
+            //== (igual)
+            //!= (no igual)
+            //>= (mayor o igual)
+            //<= (menor o igual)
+            //> (mayor que)
+            //< (menor que)
+            //&& (AND)
+            //|| (OR)
+
+            int number = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+                // Hacer algo 10 veces
+            }
+
+            while (number != 10)
+            {
+                number++;
+            }
+
+            int[] numbers = new int[5] { 1, 2, 3, 4, 5 };
+            foreach (int num in numbers)
+            {
+
+            }
+        }
+
+        static void Switch() {
+            int number = 0;
+
+            switch (number)
+            {
+                case 0:
+                    //Hacer algo
+                    break;
+                case 10:
+                    //Hacer otra cosa
+                    break;
+                default:
+                    //Hacer otra cosa
+                    break;
+            }
+        }
+    }
+}

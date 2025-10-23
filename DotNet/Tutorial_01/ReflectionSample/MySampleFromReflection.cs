1c0650e7e645eb6f17f2c20d297137e40f08e5e3
Ivan-PR <ipr.prieto@gmail.com>
2025-10-23 20:28:54 +0200

    System.Reflection: Generalidades

---
+++ DotNet/Tutorial_01/ReflectionSample/MySampleFromReflection.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionSample
+{
+    [My(Count = 10)]
+    public class MySampleFromReflection
+    {
+        public string Name { get; set; }
+        public int Number { get; set; }
+    }
+}
---
+++ DotNet/Tutorial_01/ReflectionSample/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionSample
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            new MySampleFromReflection();
+        }
+
+        private static void DoSomethingWithMyClass<T>() where T : new()
+        {
+            Activator.CreateInstance<T>();
+        }
+
+        private static void SampleWithAttributes()
+        {
+            MySampleFromReflection sample = new MySampleFromReflection();
+            MyAttribute attr = (MyAttribute)sample.GetType().GetCustomAttributes(false).FirstOrDefault();
+        }
+    }
+}

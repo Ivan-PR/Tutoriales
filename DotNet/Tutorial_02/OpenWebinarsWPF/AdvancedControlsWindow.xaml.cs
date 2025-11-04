052fe1fe2e9ba609a0b74dbccba4f34beaa8cf0e
Ivan-PR <ipr.prieto@gmail.com>
2025-11-04 17:22:35 +0100

    Creando una APP con WPF - Controles avanzados

---
+++ DotNet/Tutorial_02/OpenWebinarsWPF/AdvancedControlsWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace OpenWebinarsWPF
+{
+    /// <summary>
+    /// Lógica de interacción para AdvancedControlsWindow.xaml
+    /// </summary>
+    public partial class AdvancedControlsWindow : Window
+    {
+        private List<Car> cars = new List<Car>();
+
+        public AdvancedControlsWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            SampleList.ItemsSource=cars;
+        }
+    }
+}
---
+++ DotNet/Tutorial_02/OpenWebinarsWPF/Car.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenWebinarsWPF
+{
+    public class Car
+    {
+        public string Name { get; set; }
+        public string Maker { get; set; }
+    }
+}

763512ae511387c0833a631a1b4af89562d9464f
Ivan-PR <ipr.prieto@gmail.com>
2025-11-04 19:41:58 +0100

    Creando una APP con WPF - Animaciones con WPF

---
+++ DotNet/Tutorial_02/OpenWebinarsWPF/AnimationSampleWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace OpenWebinarsWPF
+{
+    /// <summary>
+    /// Lógica de interacción para AnimationSampleWindow.xaml
+    /// </summary>
+    public partial class AnimationSampleWindow : Window
+    {
+        public AnimationSampleWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonAnimation_Click(object sender, RoutedEventArgs e)
+        {
+            DoubleAnimation doubleAnimation = new DoubleAnimation();
+            doubleAnimation.From = 1;
+            doubleAnimation.To = 0;
+
+            ButtonAnimation.BeginAnimation(Button.OpacityProperty, doubleAnimation);
+        }
+    }
+}

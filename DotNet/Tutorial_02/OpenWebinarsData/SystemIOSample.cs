7987039cfaebaad33c42b9964687dd98f9f9a708
Ivan-PR <ipr.prieto@gmail.com>
2025-11-11 18:24:06 +0100

    Trabajando con datos - System.IO

---
+++ DotNet/Tutorial_02/OpenWebinarsData/SystemIOSample.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace OpenWebinarsData
+{
+    public static class SystemIOSample
+    {
+        public static void SampleFile()
+        {
+            if (File.Exists("C:\\MyDirectory\\MyFile.txt"))
+            {
+            }
+        }
+
+        public static void SampleDirectory()
+        {
+            Directory.Move("C:\\MyDirectory\\MyFile.txt", "D:\\");
+        }
+
+        public static void FileInfoDirectoryInfoSample()
+        {
+            FileInfo fileInfo = new FileInfo("C:\\MyDirectory\\MyFile.txt");
+            DirectoryInfo dirInfo = new DirectoryInfo("C:\\MyDirectory");
+        }
+    }
+}

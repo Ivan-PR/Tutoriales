ea401f68711198201a7edfc9e61fe49cc27c9c9a
Ivan-PR <ipr.prieto@gmail.com>
2025-11-11 02:10:36 +0100

    MVVM - Funcionalidades basicas de ReactiveUI

---
+++ DotNet/Tutorial_02/OpenWebinarsMVVM/View/Base/BaseView.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ReactiveUI;
+
+namespace OpenWebinarsMVVM.View.Base
+{
+    public class BaseView<TViewModel> : ReactiveWindow<TViewModel> where TViewModel : class
+    {
+    }
+}
--- DotNet/Tutorial_02/OpenWebinarsMVVM/View/LoginView.xaml.cs
+++ DotNet/Tutorial_02/OpenWebinarsMVVM/View/LoginView.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reactive.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,18 +13,38 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using OpenWebinarsMVVM.ViewModel;
+using ReactiveUI;
 
 namespace OpenWebinarsMVVM.View
 {
     /// <summary>
     /// Lógica de interacción para LoginView.xaml
     /// </summary>
-    public partial class LoginView : Window
+    public partial class LoginView
     {
         public LoginView()
         {
             InitializeComponent();
-            DataContext = new LoginViewModel();
+            this.WhenActivated(d =>
+            {
+                d(this.Bind(ViewModel, vm => vm.UserName, v => v.TextBoxUsername.Text));
+                d(this.Bind(ViewModel, vm => vm.PassWord, v => v.TextBoxPassword.Text));
+                d(this.BindCommand(ViewModel, vm => vm.DoLoginCommand, v => v.ButtonLogin));
+            });
+
+            this.WhenAnyValue(v => v.ViewModel.UserName, v => v.ViewModel.PassWord)
+                .Where(x => !string.IsNullOrEmpty(x.Item1))
+                .Select(x => x.Item1)
+                .Subscribe(username =>
+                {
+                    if (!string.IsNullOrEmpty(username))
+                    {
+                        TextBoxUsername.Background = new SolidColorBrush(Colors.Aquamarine);
+                    }
+                });
+
+            var textEvent = Observable.FromEventPattern<TextChangedEventArgs>(TextBoxUsername, nameof(TextBoxUsername.PreviewTextInput));
+            textEvent.Subscribe();
         }
     }
 }

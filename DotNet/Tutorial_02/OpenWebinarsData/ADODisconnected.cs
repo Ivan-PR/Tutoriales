94ca11b0681733979fc7c4199b245a6fdeac7a4e
Ivan-PR <ipr.prieto@gmail.com>
2025-11-11 20:41:21 +0100

    Trabajando con datos - ADO .NET desconectado

---
+++ DotNet/Tutorial_02/OpenWebinarsData/ADODisconnected.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Odbc;
+using System.Data;
+
+namespace OpenWebinarsData
+{
+    public class ADODisconnected
+    {
+        public void Connection()
+        {
+            OdbcConnection connection = new OdbcConnection(string.Empty);
+
+            try
+            {
+                connection.Open();
+                OdbcCommand com = connection.CreateCommand();
+                com.CommandText = "SELECT Param1, Param2, Param3 FROM Table1";
+
+                OdbcDataAdapter dataAdapter = new OdbcDataAdapter(com);
+                DataSet ds = new DataSet();
+                dataAdapter.Fill(ds);
+                connection.Close();
+            }
+            catch (Exception e)
+            {
+            }
+        }
+    }
+}

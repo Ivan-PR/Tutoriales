f6a10f8fc0be81b44b067823d5eba874271a85a5
Ivan-PR <ipr.prieto@gmail.com>
2025-11-11 20:20:13 +0100

    Trabajando con datos - Serializacion de datos

---
+++ DotNet/Tutorial_02/OpenWebinarsData/Actor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace OpenWebinarsData
+{
+    public class Actor
+    {
+        [JsonProperty("Nombre")]
+        public string Name { get; set; }
+
+        [JsonIgnore]
+        public bool Theatre { get; set; }
+
+        public int Age { get; set; }
+    }
+}
--- DotNet/Tutorial_02/OpenWebinarsData/Program.cs
+++ DotNet/Tutorial_02/OpenWebinarsData/Program.cs
@@ -3,13 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace OpenWebinarsData
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            Actor actor = new Actor();
+            actor.Age = 20;
+            actor.Theatre = false;
+            actor.Name = "Fulanito";
+
+            string json = JsonConvert.SerializeObject(actor);
         }
     }
 }

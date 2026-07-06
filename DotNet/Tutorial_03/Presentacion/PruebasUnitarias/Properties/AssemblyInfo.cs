798921af9afdb021b984933b05e14b16b500da03
Ivan-PR <ipr.prieto@gmail.com>
2026-07-06 21:39:13 +0200

    --

---
+++ DotNet/Tutorial_03/Presentacion/PruebasUnitarias/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PruebasUnitarias")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PruebasUnitarias")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b25350aa-ada5-4e78-81bb-8b7c3bca31e3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ DotNet/Tutorial_03/Presentacion/PruebasUnitarias/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Negocio;
+
+namespace PruebasUnitarias
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            try
+            {
+                Negocio.EntitiesDTO.LibrosDTO libroEjemplo = new Negocio.EntitiesDTO.LibrosDTO();
+                libroEjemplo.Nombre = "Prueba";
+                libroEjemplo.Autor = "Ejemplo";
+                libroEjemplo.FechaPublicacion = DateTime.Now;
+                libroEjemplo.idCategoria = 2;
+
+                new Negocio.Management.LibroManagement().AltaLibro(libroEjemplo);
+            }
+            catch (Exception)
+            {
+                Assert.IsTrue(false, "EL ALTA HA SIDO INCORRECTA");
+                throw;
+            }
+        }
+    }
+}
